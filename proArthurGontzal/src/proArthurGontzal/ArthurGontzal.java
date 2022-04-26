c362383a51753fe243e4ba37ba568f11b10a5c7b
Arthur <Arthur@192.168.1.35>
2022-04-26 19:55:59 +0200

    Cuarto Cambio

--- proArthurGontzal/src/proArthurGontzal/ArthurGontzal.java
+++ proArthurGontzal/src/proArthurGontzal/ArthurGontzal.java
@@ -3,7 +3,7 @@ package proArthurGontzal;
 public class ArthurGontzal {
 
 	public static void main(String[] args) {
-		System.out.println("Tercer cambio");
+		System.out.println("Cuarto Cambio Arthur");
 
 	}
 
